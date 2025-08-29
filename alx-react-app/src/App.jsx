edf044e32ef55af2cbd2e7524ca2c856ffa488fd
Osama Naeim <osamanaiem8@gmail.com>
2025-08-29 05:43:13 +0300

    Task 2: Add Header, MainContent, Footer components and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// Import the WelcomeMessage component
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* Display our custom JSX component */}
+      <Header />
+      <MainContent />
+
       <WelcomeMessage />
 
       <div>
@@ -22,7 +26,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,9 +37,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Footer />
     </>
   )
 }
