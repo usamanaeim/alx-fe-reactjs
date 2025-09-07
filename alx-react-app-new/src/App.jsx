2d055f175324a2978efb7cb46a9d33d6f742309d
Osama Naeim <osamanaiem8@gmail.com>
2025-09-07 15:21:42 +0300

    Update App.jsx to include Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
@@ -19,32 +15,13 @@ function App() {
 
       <WelcomeMessage />
 
-      {/* Use UserProfile with props */}
-      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <UserProfile
+        name="Alice"
+        age={25}
+        bio="Loves hiking and photography"
+      />
 
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Counter />
 
       <Footer />
     </>
