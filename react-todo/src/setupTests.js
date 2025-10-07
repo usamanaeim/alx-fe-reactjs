c857f3590d52538e773826959879ed6ab86879b0
Osama Naeim <osamanaiem8@gmail.com>
2025-10-08 02:45:18 +0300

    feat: add TodoList component + tests (Jest + RTL)

--- react-todo/src/setupTests.js
+++ react-todo/src/setupTests.js
@@ -1,3 +1,3 @@
 // src/setupTests.js
-// Use require to avoid ESM/CommonJS issues in Jest environment
-require('@testing-library/jest-dom/extend-expect');
+// Load jest-dom matchers so tests can use e.g. toBeInTheDocument()
+require('@testing-library/jest-dom');
